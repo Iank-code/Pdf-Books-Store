baad766b12d8ca828569c1079454278e8bb066f2
Iank-code <kian99564@gmail.com>
2024-09-28 03:17:26 +0300

    display file size

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def human_readable_file_size(size_in_bytes)
+    case size_in_bytes
+    when 0..1023
+      "#{size_in_bytes} Bytes"
+    when 1024..(1024 * 1024 - 1)
+      "#{(size_in_bytes / 1024.0).round(2)} KB"
+    when (1024 * 1024)..(1024 * 1024 * 1024 - 1)
+      "#{(size_in_bytes / (1024.0 * 1024)).round(2)} MB"
+    else
+      "#{(size_in_bytes / (1024.0 * 1024 * 1024)).round(2)} GB"
+    end
+  end
 end
